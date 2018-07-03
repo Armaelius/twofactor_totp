2ada14135e0f260a3da677873b72eac0a39ac815
Armaelius <38795882+Armaelius@users.noreply.github.com>
2018-07-03 03:24:27 +0300

    Update ru.js
    "Аутентификация вторым фактором" somehow does not russian at all. "Двухфакторная аутентификация" is much better.

--- l10n/ru.js
+++ l10n/ru.js
@@ -18,6 +18,6 @@ OC.L10N.register(
     "A Two-Factor-Auth Provider for TOTP (RFC 6238)" : "Провайдер двухфакторной аутентификации для TOTP (RFC 6238)",
     "Submit" : "Отправить",
     "Get the authentication code from the two-factor authentication app on your device." : "Получите аутентификационный код из приложения двухфакторной аутентификации на вашем устройстве.",
-    "TOTP second-factor auth" : "Аутентификация вторым фактором с помощью TOTP"
+    "TOTP second-factor auth" : "Двухфакторная аутентификация с помощью TOTP"
 },
 "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
